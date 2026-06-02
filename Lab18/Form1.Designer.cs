33860d70704cb47fcd7c542dad92647edd5e97fe
Daniil-1178 <daniilzubkov278@gmail.com>
2026-06-03 02:26:02 +0300

    Add files via upload

---
+++ Lab18/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvProcesses = new System.Windows.Forms.ListView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuThreadsModules = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKill = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvProcesses
+            // 
+            this.lvProcesses.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvProcesses.FullRowSelect = true;
+            this.lvProcesses.HideSelection = false;
+            this.lvProcesses.Location = new System.Drawing.Point(217, 33);
+            this.lvProcesses.MultiSelect = false;
+            this.lvProcesses.Name = "lvProcesses";
+            this.lvProcesses.Size = new System.Drawing.Size(331, 326);
+            this.lvProcesses.TabIndex = 0;
+            this.lvProcesses.UseCompatibleStateImageBehavior = false;
+            this.lvProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(323, 365);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(111, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Оновити процеси";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuInfo,
+            this.menuThreadsModules,
+            this.menuKill,
+            this.menuExport});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(193, 92);
+            // 
+            // menuInfo
+            // 
+            this.menuInfo.Name = "menuInfo";
+            this.menuInfo.Size = new System.Drawing.Size(192, 22);
+            this.menuInfo.Text = "Детальна інформація";
+            this.menuInfo.Click += new System.EventHandler(this.menuInfo_Click);
+            // 
+            // menuThreadsModules
+            // 
+            this.menuThreadsModules.Name = "menuThreadsModules";
+            this.menuThreadsModules.Size = new System.Drawing.Size(192, 22);
+            this.menuThreadsModules.Text = "Потоки та модулі";
+            this.menuThreadsModules.Click += new System.EventHandler(this.menuThreadsModules_Click);
+            // 
+            // menuKill
+            // 
+            this.menuKill.Name = "menuKill";
+            this.menuKill.Size = new System.Drawing.Size(192, 22);
+            this.menuKill.Text = "Зупинити процес";
+            this.menuKill.Click += new System.EventHandler(this.menuKill_Click);
+            // 
+            // menuExport
+            // 
+            this.menuExport.Name = "menuExport";
+            this.menuExport.Size = new System.Drawing.Size(192, 22);
+            this.menuExport.Text = "Експортувати список";
+            this.menuExport.Click += new System.EventHandler(this.menuExport_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Назва процесу";
+            this.columnHeader1.Width = 220;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID (Ідентифікатор)";
+            this.columnHeader2.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lvProcesses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvProcesses;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem menuInfo;
+        private System.Windows.Forms.ToolStripMenuItem menuThreadsModules;
+        private System.Windows.Forms.ToolStripMenuItem menuKill;
+        private System.Windows.Forms.ToolStripMenuItem menuExport;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Lab18/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lab18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureListView();
+            LoadProcesses();
+        }
+
+        private void ConfigureListView()
+        {
+            lvProcesses.View = View.Details;
+            lvProcesses.FullRowSelect = true;
+            lvProcesses.MultiSelect = false;
+
+            lvProcesses.Columns.Add("Назва процесу", 220);
+            lvProcesses.Columns.Add("PID (ідентифікатор)", 100);
+
+            lvProcesses.ContextMenuStrip = contextMenu;
+        }
+
+        private void LoadProcesses()
+        {
+            lvProcesses.Items.Clear();
+
+            Process[] processList = Process.GetProcesses();
+
+            foreach (Process proc in processList)
+            {
+                ListViewItem item = new ListViewItem(proc.ProcessName);
+
+                item.SubItems.Add(proc.Id.ToString());
+
+                item.Tag = proc;
+
+                lvProcesses.Items.Add(item);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void menuInfo_Click(object sender, EventArgs e)
+        {
+            if (lvProcesses.SelectedItems.Count == 0) return;
+
+            Process selectedProc = (Process)lvProcesses.SelectedItems[0].Tag;
+
+            try
+            {
+                string info = $"Ім'я процесу: {selectedProc.ProcessName}\n" +
+                              $"ID процесу (PID): {selectedProc.Id}\n" +
+                              $"Обсяг віртуальної пам'яті: {selectedProc.VirtualMemorySize64 / 1024 / 1024} МБ\n" +
+                              $"Час запуску: {selectedProc.StartTime.ToLongTimeString()}";
+
+                MessageBox.Show(info, "Деталі процесу", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не вдалося отримати повну інформацію (обмеження ОС): {ex.Message}",
+                                "Помилка доступу", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void menuThreadsModules_Click(object sender, EventArgs e)
+        {
+            if (lvProcesses.SelectedItems.Count == 0) return;
+
+            Process selectedProc = (Process)lvProcesses.SelectedItems[0].Tag;
+            string details = $"ПОТОКИ ПРОЦЕСУ ({selectedProc.ProcessName})\n";
+
+            try
+            {
+                int threadCount = 0;
+                foreach (ProcessThread thread in selectedProc.Threads)
+                {
+                    if (threadCount++ < 10)
+                        details += $"ID Потоку: {thread.Id} | Поточний пріоритет: {thread.CurrentPriority}\n";
+                }
+
+                details += $"\nЗАВАНТАЖЕНІ МОДУЛІ (DLL)\n";
+                int moduleCount = 0;
+
+                foreach (ProcessModule module in selectedProc.Modules)
+                {
+                    if (moduleCount++ < 10)
+                        details += $"Модуль: {module.ModuleName}\nШлях: {module.FileName}\n\n";
+                }
+
+                MessageBox.Show(details, "Потоки та модулі процесу", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка зчитування внутрішніх структур: {ex.Message}",
+                                "Обмеження безпеки ОС", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void menuKill_Click(object sender, EventArgs e)
+        {
+            if (lvProcesses.SelectedItems.Count == 0) return;
+
+            Process selectedProc = (Process)lvProcesses.SelectedItems[0].Tag;
+
+            var confirmResult = MessageBox.Show($"Ви впевнені, що хочете примусово завершити процес {selectedProc.ProcessName}?",
+                                                 "Підтвердження дії", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirmResult == DialogResult.Yes)
+            {
+                try
+                {
+                    selectedProc.Kill();
+                    MessageBox.Show("Процес успішно зупинено", "Операція завершена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadProcesses();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Не вдалося завершити процес: {ex.Message}", "Помилка виконання", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void menuExport_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Текстові файли (.txt)|.txt";
+                sfd.Title = "Збереження списку процесів у файл";
+                sfd.FileName = "System_Processes_Report.txt";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamWriter writer = new StreamWriter(sfd.FileName))
+                        {
+                            writer.WriteLine($"Звіт про запущені процеси системи від: {DateTime.Now}");
+                            writer.WriteLine(new string('=', 60));
+                            writer.WriteLine($"{"PID",-12} | {"Назва активного процесу"}");
+                            writer.WriteLine(new string('=', 60));
+
+                            foreach (ListViewItem item in lvProcesses.Items)
+                            {
+                                string name = item.Text;
+                                string pid = item.SubItems[1].Text;
+
+                                writer.WriteLine($"{pid,-12} | {name}");
+                            }
+                        }
+                        MessageBox.Show("Експорт звіту успішно завершено!", "Файл збережено", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Помилка запису на диск: {ex.Message}", "Критична помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
